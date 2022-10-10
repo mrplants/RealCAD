689721611b25bf7dd877423b880e172b926be258
Sean F <fitzgerald.sean.t@gmail.com>
2022-10-10 13:04:36 -0400

    Initial Commit

---
+++ RealCAD/RealCADApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealCADApp.swift
+//  RealCAD
+//
+//  Created by Sean Fitzgerald on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RealCADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
