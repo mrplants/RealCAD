b68df2f15f79bcacf31d7899eccea0eb2b74a675
Sean F <fitzgerald.sean.t@gmail.com>
2022-10-10 15:48:41 -0400

    Began creating AugmentedRealityView.  Removed pre-filled TextView.

---
+++ RealCAD/ARView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RealCAD
+//
+//  Created by Sean Fitzgerald on 10/10/22.
+//
+
+import SwiftUI
+
+struct ARView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+    }
+}
+
+struct ARView_Previews: PreviewProvider {
+    static var previews: some View {
+        ARView()
+    }
+}
